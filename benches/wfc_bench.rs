391a69425c3c99c1c51c51b8ea3d5c4b229242aa
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2023-01-26 13:05:37 +0100

    add benchmark for testing fibheap only

--- benches/wfc_bench.rs
+++ benches/wfc_bench.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
 use wfc::wfc::{rules, vec3d::Vec3D, baseline, traits::WFC, queueprop, queueprop_bitarrayset, queueprop_bitarrayset_fibheap, stackprop};
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn criterion_benchmark_all(c: &mut Criterion) {
     let mut group = c.benchmark_group("wfc");
     let rules = rules::get_pipes_rules();
     for size in 4..50
@@ -25,6 +25,12 @@ fn criterion_benchmark(c: &mut Criterion) {
                 &input,
                 |b, i| b.iter(|| black_box(
                     queueprop::QueueProp::solve(&i, &rules)
+            )));
+            group.bench_with_input(
+                BenchmarkId::new("stackprop", size),
+                &input,
+                |b, i| b.iter(|| black_box(
+                    stackprop::StackProp::solve(&i, &rules)
             )));
         }
         group.bench_with_input(
@@ -67,5 +73,21 @@ fn criterion_benchmark_queue_stack(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark, criterion_benchmark_queue_stack);
+fn criterion_benchmark_fibheap(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fibheap_only");
+    let rules = rules::get_pipes_rules();
+    let size = 40;
+    let input = Vec3D::with_borders(size, size, size, rules::EMPTY, rules::BORDER);
+    group.sample_size(10);
+    group.throughput(Throughput::Elements(size.pow(3) as u64));
+    group.bench_with_input(
+        BenchmarkId::new("fibheap", size),
+        &input,
+        |b, i| b.iter(|| black_box(
+            queueprop_bitarrayset_fibheap::QueuePropBitArraySetFibHeap::solve(&i, &rules)
+    )));
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark_all, criterion_benchmark_queue_stack, criterion_benchmark_fibheap);
 criterion_main!(benches);
