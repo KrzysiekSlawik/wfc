221593986303f91967bc99e6a907f061e1652d83
KrzysiekSlawik <krzysiek.slawik@gmail.com>
2023-02-02 22:27:17 +0100

    add csv printing for fibheap wfc in example target

--- queueprop_bitarrayset_fibheap_example/main.rs
+++ queueprop_bitarrayset_fibheap_example/main.rs
@@ -2,13 +2,16 @@ use wfc::wfc::{vec3d::{Vec3D, PosIter3D}, rules, queueprop_bitarrayset_fibheap, 
 
 fn main()
 {
-    let size:usize = 35;
+    let size:usize = 15;
     let map = Vec3D::with_borders(size, size, size, rules::EMPTY, rules::BORDER);
     let rules = rules::get_pipes_rules();
     let solution = queueprop_bitarrayset_fibheap::QueuePropBitArraySetFibHeap::solve(&map, &rules);
     //do something with solution so it is not optimised out
+    let mut csv = "".to_owned();
     for (x, y, z) in PosIter3D::new(&map)
     {
-        println!("DUMMY anty optimise! {}", solution.get(x, y, z));
+        csv.push_str(solution.get(x, y, z).to_string().as_str());
+        csv.push(',');
     }
+    println!("{}", csv);
 }
